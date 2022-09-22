92536d62dd311722807c05c8da26d8bcf0f3138c
Jéssica Roberta <jessicaroberta@MacBook-Air-de-Jessica.local>
2022-09-22 10:35:02 -0300

    Fix bug of ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 //yarn add @types/react-router-dom -D
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
+    //yarn add react-router-dom@6
     <BrowserRouter>
-      <Route path='/' element= {<Home/>} />
-      <Route path="/rooms/new" element={<NewRoom/>} />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/rooms/new" element={<NewRoom/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
